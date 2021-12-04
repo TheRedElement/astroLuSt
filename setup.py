7e2bbf8250e83b1dcaee4a419db4a3ea9bad2ce0
TheRedElement <lukas.steinwender99@gmail.com>
2021-12-04 09:17:32 +0100

    changed setup.py

--- setup.py
+++ setup.py
@@ -5,31 +5,16 @@
 # https://github.com/pypa/sampleproject
 
 
-from setuptools import setup, find_packages
-import pathlib
-
-here = pathlib.Path(__file__).parent.resolve()
-
-# Get long description from the README file
-long_description = (here / "README.md").read_text(encoding="utf-8")
+from setuptools import setup
 
 setup(
     name="astroLuSt",
     version="0.0.1",
-    description="Module containing tools useful especially in astronomy.",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     url="https://github.com/TheRedElement/astroLuSt",
     author="Steinwender Lukas",
     author_email="lukas.steinwender99@gmail.com",
-    classifiers=[
-        "Intended Audience :: Science/Research",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Scientific/Engineering :: Astronomy",
-        "Natural Language :: English",
-        "Development Status :: 2 - Pre-Alpha"
-    ],
-    keywords="Astronomy, Dataanalysis",
+    description="Module containing code especially useful in astronomy.",
+    long_description="Module containing code especially useful in astronomy.",
     packages=["astroLuSt"],
     install_requires=[
         "numpy",
@@ -37,10 +22,49 @@ setup(
         "scipy",
         "datetime",
     ],
-    data_files=[
-        ('needed_files', ['files/Colorcodes.txt'])
-    ],
-    project_urls={
-        "Source: https://github.com/TheRedElement/astroLuSt" 
-    },
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Topic :: astronomy, datanalysis",
+    ]
+
 )
+
+# from setuptools import setup, find_packages
+# import pathlib
+
+# here = pathlib.Path(__file__).parent.resolve()
+
+# # Get long description from the README file
+# long_description = (here / "README.md").read_text(encoding="utf-8")
+
+# setup(
+#     name="astroLuSt",
+#     version="0.0.1",
+#     description="Module containing tools useful especially in astronomy.",
+#     long_description=long_description,
+#     long_description_content_type='text/markdown',
+#     url="https://github.com/TheRedElement/astroLuSt",
+#     author="Steinwender Lukas",
+#     author_email="lukas.steinwender99@gmail.com",
+#     classifiers=[
+#         "Intended Audience :: Science/Research",
+#         "Programming Language :: Python :: 3.8",
+#         "Topic :: Scientific/Engineering :: Astronomy",
+#         "Natural Language :: English",
+#         "Development Status :: 2 - Pre-Alpha",
+#     ],
+#     keywords="Astronomy, Dataanalysis",
+#     packages=["astroLuSt"],
+#     install_requires=[
+#         "numpy",
+#         "matplotlib",
+#         "scipy",
+#         "datetime",
+#     ],
+#     data_files=[
+#         ('needed_files', ['files/Colorcodes.txt']),
+#     ],
+#     project_urls={
+#         "Source: https://github.com/TheRedElement/astroLuSt" 
+#     },
+# )
