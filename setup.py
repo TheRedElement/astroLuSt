457ae61bbe8d24e9c36ab1fdd926af83933a3513
TheRedElement <lukas.steinwender99@gmail.com>
2021-12-04 10:48:29 +0100

    changed to exclude PHOEBE_astroLuSt.py at distribution

--- setup.py
+++ setup.py
@@ -35,8 +35,8 @@ setup(
     ],
     keywords="Astronomy, Dataanalysis",
     # package_dir={"": "astroLuSt"},
-    # packages=["astroLuSt"],
-    packages=find_packages(exclude=["astroLuSt/__pycache__", "astroLuSt/PHOEBE_astro_LuSt.py"]),
+    packages=["astroLuSt"],
+    # packages=find_packages(exclude=["astroLuSt/__pycache__", "astroLuSt/PHOEBE_astro_LuSt.py"]),
     # python_requires=">=3.8, < 4",
     install_requires=[
         "numpy",
