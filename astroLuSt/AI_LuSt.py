5d867d677495bc127183c995faa8b908c64b6b35
TheRedElement <lukas.steinwender99@gmail.com>
2022-04-21 15:20:30 +0200

    updated AI_LuSt.py

--- astroLuSt/AI_LuSt.py
+++ astroLuSt/AI_LuSt.py
@@ -1,5 +1,112 @@
 #TODO: create class to store functions
 
+def plot_confusion_matrix(
+    vals, classes,
+    xlab="Class", ylab="Class", cbarlabel="", cmap="viridis",
+    annotate=True, annotationcolor="w", textfontsize=None,
+    figsize=(9,5), fontsize=16,
+    save=False):
+    #TODO: Finish documentation
+    """
+        - function to plot a confusion matrix
+
+        Parameters
+        ----------
+            - vals
+                - np.array
+                    - 2d array
+                    - value assigned to each pair of classes
+            - classes
+                - list, np.array
+                    - 1d array
+                - classes the values got calculated for
+            - xlab
+                - str, optional
+                - xaxis label
+                - the default is 'Class'
+            - ylab
+                - str, optional
+                - xaxis label
+                - the default is 'Class'
+            - cbarlab
+                - str, optional
+                - colorbar label
+                - the default is ''
+            - cmap
+                - str, optional
+                - matplotlib colormap to use
+                - the default is 'viridis'
+            - annotate
+                - bool, optional
+                - whether to depict the heatmap values in the individiual cells
+                - the default is true
+            - annotationcolor
+                - str, optional
+                - color of the heatmapvalues, when depicted in the cells
+                - the default is "w"
+            - annotationfontsize
+                - float, optional
+                - fontsize of the heatmapvalues, when depicted in the cells
+                - the default is None
+                    - will result in the same value as 'fontsize'
+            - figsize
+                - tuple, optional
+                - size of the figure
+                - the default is (9,5)
+            - fontsize
+                - float, optional
+                - fontsize of labels
+                - the default is 16
+            - save
+                - str, optional
+                - location of where to save the plot
+                - the default is False
+                    - will not save the plot
+    """
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    if textfontsize is None:
+        textfontsize = fontsize
+
+    #generate tick positions
+    tick_positions = np.arange(0, vals.shape[0], 1)
+
+    fig = plt.figure(figsize=figsize)
+    ax = fig.add_subplot(111)
+    im = ax.imshow(vals, cmap=cmap)
+    cbar= fig.colorbar(im)
+
+    #annotating
+    if annotate:
+        for i in range(vals.shape[0]):
+            for j in range(vals.shape[1]):
+                text = ax.text(j, i, f"{vals[i, j]:.1f}",
+                            ha="center", va="center", color=annotationcolor, fontsize=textfontsize)
+
+    #set locators
+    ax.xaxis.set_major_locator(plt.MaxNLocator(vals.shape[0]))
+    ax.yaxis.set_major_locator(plt.MaxNLocator(vals.shape[0]))
+    ax.set_xticks(tick_positions)
+    ax.set_yticks(tick_positions)
+    ax.set_xticklabels(classes)
+    ax.set_yticklabels(classes)
+    
+    #fontsize
+    ax.tick_params("both", labelsize=fontsize)
+    cbar.ax.tick_params(labelsize=fontsize)
+
+    #labelling
+    cbar.set_label(cbarlabel, fontsize=fontsize)
+    ax.set_xlabel(xlab, fontsize=fontsize)
+    ax.set_ylabel(ylab, fontsize=fontsize)
+
+    if type(save) == str:
+        plt.savefig(save, dpi=180)
+
+    return fig, ax
+
+
 def kdist(X, k=4, eps_pred=None, testplot=False):
     #TODO: Autodetermine kink-distance
     """
@@ -90,12 +197,15 @@ def kdist(X, k=4, eps_pred=None, testplot=False):
 def plot_feature_spaces(
     X, y_pred=None,
     class_names=None,
+    markersize=20, alpha=.8,
     cmap="viridis",
     figtitle="Feature Spaces",
     figsize=(10,10), fontsize=16,
     save=False):
+    #NOTE: changes from last git push: markersize, alpha
     """
         - function to plot all possible feature-space combinations in a correlation-plot
+            - will result in a figure of X.shape[0]**2 subplots
 
         Parameters
         ----------
@@ -116,6 +226,14 @@ def plot_feature_spaces(
                     - can also be strings
                 - the default is None
                     - will copy the values passed to y_pred
+            - markersize
+                - float, optional
+                - the markersize in points**2
+                - the default is 20
+            - alpha
+                - float, optional
+                - the alpha-value to use for the datapoints
+                - the default is 0.8
             - cmap
                 - str, optional
                 - name of the colormap to use for coloring the different classes
@@ -181,7 +299,10 @@ def plot_feature_spaces(
         for idx2, x2 in enumerate(X.T):
 
             ax = fig.add_subplot(rows, cols, pos)
-            sctr = ax.scatter(x1, x2, c=color, cmap=cmap, vmin=y_pred.min(), vmax=y_pred.max())
+            sctr = ax.scatter(
+                x1, x2, 
+                s=markersize, alpha=alpha,
+                c=color, cmap=cmap, vmin=y_pred.min(), vmax=y_pred.max())
             ax.set_xticklabels([])
             ax.set_yticklabels([])
 
@@ -195,9 +316,13 @@ def plot_feature_spaces(
     ax0.set_xlabel("Feature i", fontsize=fontsize)
     ax0.set_ylabel("Feature j", fontsize=fontsize)
     fig.subplots_adjust(right=0.8)
-    
     cbar_ax = fig.add_axes([0.85, 0.13, 0.05, 0.8])
     cbar = fig.colorbar(sctr, cax=cbar_ax)
+
+    #to avoid lines in cbar with transparency
+    cbar.set_alpha(1)
+    cbar.draw_all()
+
     cbar.ax.set_title("Class", ha="left", pad=20, fontsize=fontsize+2)
     cbar.set_ticks(np.unique(y_pred))
     cbar.set_ticklabels(class_names)
