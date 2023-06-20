4775fe7224573d399ed64bf9af0d7e3844e3fdd6
TheRedElement <lukas.steinwender99@gmail.com>
2023-06-20 11:30:41 -0400

    updated documentation

--- astroLuSt/ai/colorsforclasses.py
+++ astroLuSt/ai/colorsforclasses.py
@@ -1,47 +1,52 @@
 
+#%%imports
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from typing import Union
 
 #%%generate colors
 
-def generate_colors(classes, vmin=None, vmax=None, vcenter=None, ncolors=None, cmap="viridis"):
-    #TODO: Include in Model_02 (i.e. the plotting_astroLuSt module)
+def generate_colors(
+    classes:Union[list,np.ndarray], vmin=None, vmax=None, vcenter=None, ncolors=None, cmap="viridis"):
     """
         - function to generate colors for a given set of unique classes
         - generates colors based on a matplotlib colormap
 
         Parameters
         ----------
-            - classes
-                - list, np.array
-                - the classes to consider
-                - does not have to consist of unique classes
-                    - the function will pick out the unique classes by itself
-            - cmap
+            - `classes`
+                - list, np.ndarray
+                - the `classes` to consider
+                - does not have to consist of unique `classes`
+                    - the function will pick out the unique `classes` by itself
+            - `cmap`
                 - str, optional
-                - name of the colormap to use for coloring the different classes
+                - name of the colormap to use for coloring the different `classes`
                 - the default is 'viridis'
-            - vmin
+            - `vmin`
                 - int, optional
-                - vmin value of the colormap
+                - `vmin` value of the colormap
                 - useful if you want to modify the class-coloring
-                - the default is None
-                    - will be set to y_pred.min()
-            - vmax
+                - the default is `None`
+                    - will be set to `y_pred.min()`
+            - `vmax`
                 - int, optional
-                - vmax value of the colormap
+                - `vmax` value of the colormap
                 - useful if you want to modify the class-coloring
-                - the default is None
-                    - will be set to y_pred.max()
-            - vcenter
+                - the default is `None`
+                    - will be set to `y_pred.max()`
+            - `vcenter`
                 - int, optional
-                - vcenter value of the colormap
+                - `vcenter` value of the colormap
                 - useful if you want to modify the class-coloring
-                - the default is None
-                    - will be set to y_pred.mean()
-            - ncolors
+                - the default is `None`
+                    - will be set to `y_pred.mean()`
+            - `ncolors`
                 - int, optional
                 - number of different colors to generate
-                - the default is None
-                    - will be set to the number of unique classes in y_pred
+                - the default is `None`
+                    - will be set to the number of unique `classes` in `y_pred`
 
 
         Raises
@@ -49,22 +54,21 @@ def generate_colors(classes, vmin=None, vmax=None, vcenter=None, ncolors=None, c
 
         Returns
         -------
-            - colors
-                - list
-                - contains as many different colors as there are unique classes in 'classes'
+            - `colors`
+                - `list`
+                - contains as many different colors as there are unique classes in `classes`
 
         Dependencies
         ------------
             - numpy
             - matplotlib
+            - typing
 
         Comments
         --------
 
     """
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import matplotlib.colors as mcolors
+
 
     classes_int = np.arange(0, np.unique(classes).shape[0], 1, dtype=int)    #initialize integer-class array
 
@@ -81,6 +85,7 @@ def generate_colors(classes, vmin=None, vmax=None, vcenter=None, ncolors=None, c
     divnorm = mcolors.TwoSlopeNorm(vmin=vmin, vcenter=vcenter, vmax=vmax)
     colors = plt.cm.get_cmap(cmap, ncolors)
     colors = colors(divnorm(np.unique(classes_int)))
+    
     return colors
 
 
