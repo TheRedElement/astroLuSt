78db6d3bd5eb3e2747235815202fdcd1719123e8
TheRedElement <lukas.steinwender99@gmail.com>
2023-06-20 17:12:59 -0400

    streamlined docstring

--- astroLuSt/preprocessing/sorting.py
+++ astroLuSt/preprocessing/sorting.py
@@ -1,7 +1,7 @@
 
 #%%imports
 import numpy as np
-from typing import List, Tuple
+from typing import Tuple
 
 #%%definitions
 
@@ -9,25 +9,25 @@ def sort_into_batches(
     X:np.ndarray, *y:np.ndarray
     ) -> Tuple[np.ndarray,...]:
     """
-        - function that takes a nested array X and creates a new array
-            - this array contains as entries arrays that contain all arrays of X which have the same length
-            - if labels (y) exist, those will be sorted accordingly
+        - function that takes a nested array `X` and creates a new array
+            - this array contains as entries arrays that contain all arrays of `X` which have the same length
+            - if labels (`y`) exist, those will be sorted accordingly
 
         Parameters
         ----------
-            - X
+            - `X`
                 - np.ndarray
                 - nested np.ndarray
                     - can contain arrays of different lenghts
                 - this array will be split up into arrays of similar length
-            - y
+            - `y`
                 - list, optional
                 - contains
                     - np.ndarrays
-                        - have to have same first axis shape as 'X'
-                - labels corresponding to 'X'
-                - will be split such that the same entries in 'X' are still paired up with the same labels
-                - the default is None
+                        - have to have same first axis shape as `X`
+                - labels corresponding to `X`
+                - will be split such that the same entries in `X` are still paired up with the same labels
+                - the default is `None`
                     - will be ignored
 
         Raises
@@ -35,22 +35,22 @@ def sort_into_batches(
 
         Returns
         -------
-            - X_batches
+            - `X_batches`
                 - np.ndarray
                 - has dtype object
                 - contains subarrays
                     - each subarray contains samples of the same length
-                - i.e. the output shape will be (nlenghts, nperlength, length, nfeatures)
-                    - 'nlenghts' is the number of unique array-lengths present in 'X'
-                    - 'nperlength' is the number of arrays that have the same length ('length')
-                    - 'length' is the common length of a batch of arrays
-                    - 'nfeatures' is the number of features present in the arrays of a batch
-            - *y_batches
+                - i.e. the output shape will be (`nlenghts`, `nperlength`, `length`, `nfeatures`)
+                    - `nlenghts` is the number of unique array-lengths present in `X`
+                    - `nperlength` is the number of arrays that have the same length (`length`)
+                    - `length` is the common length of a batch of arrays
+                    - `nfeatures` is the number of features present in the arrays of a batch
+            - `*y_batches`
                 - np.ndarray
                 - has dtype object
                 - contains subarrays
-                - split of all arrays contained within 'y' such that they are still comparabel with 'X'
-                - will return as many individual y_batches, as elements in 'y'
+                - split of all arrays contained within `y` such that they are still comparabel with `X`
+                - will return as many individual `y_batches`, as elements in `y`
 
         Dependencies
         ------------
@@ -59,14 +59,18 @@ def sort_into_batches(
 
         Comments
         --------
-            - to only transform 'X' alone call
+            - to only transform `X` alone call
                 
+                ```python
                 >>> X_batches, = sort_into_batches(X)
-            
-            - to transform a multitude of arrays (n in the example) w.r.t. 'X' use
-
+                ```
+                
+            - to transform a multitude of arrays (n in the example) w.r.t. `X` use
+                
+                ```python
                 >>> X_batches, y0_, y1_, ..., yn_ = sort_into_batches(X, y=[y0, y1, ..., yn])
-
+                ```
+                
     """
 
     lengths = [len(x) for x in X]
