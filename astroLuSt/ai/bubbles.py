0017cd064115358aae7ded8c54d056c2a7df2da7
TheRedElement <lukas.steinwender99@gmail.com>
2023-07-03 14:02:36 -0400

    made plot_result() compatible with higher dimensions. Added option to remove noise-points

--- astroLuSt/ai/bubbles.py
+++ astroLuSt/ai/bubbles.py
@@ -24,43 +24,27 @@ class BUBBLES:
         ) -> Any:
         uniques, counts = np.unique(x, return_counts=True)
         most_common = uniques[np.argmax(counts)]
+        
         return most_common
 
-
-    def nd_sphere(self,
-        x0:np.ndarray, r:float,
-        ) -> np.ndarray:
-
-        x = r*np.cos(np.linspace(0,2*np.pi,100))
-        y = r*np.sin(np.linspace(0,2*np.pi,100))
-
-        xy = np.array([x,y]).T+x0
-
-        return xy
-
-    def __rect_2d(self,
-        y_grid:np.ndarray, X_grid:np.ndarray,
+    def __rect_nd(self,
         X:np.ndarray, y:np.ndarray,
-        min_pts:int,
-        ) -> np.ndarray:
+        res, min_pts:int,
+        ) -> None:
+        
+        X_min = self.X_grid-res/2
+        X_max = self.X_grid+res/2
+        for idx, (x_min, x_max) in enumerate(zip(X_min, X_max)):
+            
+            y_bool = np.all((
+                (x_min < X)&
+                (X < x_max)
+            ), axis=1)
 
-        for idx, Xi in enumerate(X_grid):
-            y_bool
+            if y_bool.sum() > min_pts:
+                self.y_grid[idx] = self.get_most_common(y[y_bool])
 
-        # for i in range(len(X_grid)-1):
-        #     for j in range(len(X_grid[i])-1):
-        #         y_bool = (
-        #             (X[:,0] > X_grid[i  ,j  ][0]) &
-        #             (X[:,1] > X_grid[i  ,  j][1]) &
-        #             (X[:,0] < X_grid[i+1,j+1][0]) &
-        #             (X[:,1] < X_grid[i+1,j+1][1]) 
-        #         )
-
-        #         if y_bool.sum() > min_pts:
-        #             y[i, j] = self.get_most_common(y[y_bool])
-
-
-        return y_grid
+        return
 
     def __sphere_nd(self,
         X:np.ndarray, y:np.ndarray,
@@ -74,7 +58,7 @@ class BUBBLES:
 
             if y_bool.sum() > min_pts:
                 self.y_grid[idx] = self.get_most_common(y[y_bool])
-        
+
         return
 
     def fit(self,
@@ -97,9 +81,9 @@ class BUBBLES:
         print(f'y_grid.shape: {self.y_grid.shape}')
 
 
-        # self.y_grid = self.__rect_nd(
+        # self.__rect_nd(
         #     X=X, y=y,
-        #     min_pts=min_pts,
+        #     res=res, min_pts=min_pts,
         # )
         self.__sphere_nd(
             X=X, y=y,
@@ -124,17 +108,33 @@ class BUBBLES:
     def plot_result(self,
         X:np.ndarray=None, y:np.ndarray=None,
         dims:Union[list,slice]=None,
+        remove_noise:bool=False,
         ) -> Tuple[Figure,plt.Axes]:
 
+
+        cmap = 'Set2'
+        if self.X_grid.shape[1] < 3: projection = None
+        else:                        projection = '3d'
+
         fig = plt.figure()
-        ax1 = fig.add_subplot(111, projection='3d')
+        ax1 = fig.add_subplot(111, projection=projection)
 
-        sctr1 = ax1.scatter(*self.X_grid[:,dims].T, c=self.y_grid, alpha=0.1, s=10)
+        
+        # if self.X_grid.shape[1] == 2:
+        #     # mappable = ax1.contourf(*self.X_grid.T, self.y_grid)
+        #     mappable = ax1.tricontourf(*self.X_grid.T, self.y_grid)
+        
+        if remove_noise:
+            mappable = ax1.scatter(*self.X_grid[(self.y_grid!=-1)].T[dims], c=self.y_grid[(self.y_grid!=-1)], alpha=0.5, s=10, vmin=-1, cmap=cmap)
+        else:
+            mappable = ax1.scatter(*self.X_grid[:].T[dims],                 c=self.y_grid,                    alpha=0.5, s=10, vmin=-1, cmap=cmap)
+        
+
         if X is not None:
-            sctr2 = ax1.scatter(*X[:,:3].T, c=y, alpha=0.5, s=50, ec='w', vmin=-1)
+            ax1.scatter(*X[:,dims].T, c=y, alpha=0.5, s=50, ec='w', vmin=-1, cmap=cmap)
 
 
-        fig.colorbar(sctr1)
+        fig.colorbar(mappable)
 
         axs = fig.axes
 
