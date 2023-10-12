dd16ba127960b99fa81e9bc544761c426d42cd4f
TheRedElement <lukas.steinwender99@gmail.com>
2023-10-12 14:52:33 +0200

    modified parameters. Enabled calculation without reference mag and flux.

--- astroLuSt/physics/photometry.py
+++ astroLuSt/physics/photometry.py
@@ -67,16 +67,16 @@ def wesenheit_magnitude(
     return w
 
 def mags2fluxes(
-    mag:Union[np.ndarray,float],
+    m:Union[np.ndarray,float],
     m_ref:Union[np.ndarray,float],
-    f_ref:Union[np.ndarray,float],
+    f_ref:Union[np.ndarray,float]=1,
     ) -> Union[np.ndarray,float]:
     """
         - function to convert magnitudes to flux
         
         Parameters
         ----------
-            - `mag`
+            - `m`
                 - float, np.ndarray
                 - magnitudes to be converted
             - `m_ref`
@@ -84,19 +84,21 @@ def mags2fluxes(
                 - reference magnitude for the conversion
                     - this value is dependent on the passband in use
             - `f_ref`
-                - float, np.ndarray
+                - float, np.ndarray, optional
                 - reference flux for the conversion
                     - corresponding to `m_ref`
                     - this value is dependent on the passband in use
+                - the default is 1
+                    - will return the fraction `f/f_ref`
 
         Raises
         ------
 
         Returns
         -------
-            - `flux`
+            - `f`
                 - float, np.array
-                - flux corresponding to `mag`
+                - flux corresponding to `m`
 
         Dependencies
         ------------
@@ -107,40 +109,42 @@ def mags2fluxes(
         --------
 
     """
-    flux = 10**(-0.4*(mag - m_ref)) + f_ref
-    return flux
+    f = 10**(-0.4*(m - m_ref)) * f_ref
+    return f
 
 def fluxes2mags(
-    flux:Union[np.ndarray,float],
-    m_ref:Union[np.ndarray,float],
+    f:Union[np.ndarray,float],
     f_ref:Union[np.ndarray,float],
+    m_ref:Union[np.ndarray,float]=0,
     ) -> Union[np.ndarray,float]:
     """
         - function to convert photon flux to magnitudes
 
         Parameters
         ----------
-            - `flux`
+            - `f`
                 - float, np.array
                 - fluxes to be converted
-            - `m_ref`
-                - float, np.ndarray
-                - reference magnitude for the conversion
-                    - this value is dependent on the passband in use
             - `f_ref`
                 - float, np.ndarray
                 - reference flux for the conversion
-                    - corresponding to `m_ref`
                     - this value is dependent on the passband in use
+            - `m_ref`
+                - float, np.ndarray, optional
+                - reference magnitude for the conversion
+                    - corresponding to `f_ref`
+                    - this value is dependent on the passband in use
+                - the default is 0
+                    - will return the difference `m - m_ref`
 
         Raises
         ------
 
         Returns
         -------
-            - `mags`
+            - `m`
                 - float, np.array
-                - magnitudes corresponding to `flux`
+                - magnitudes corresponding to `f`
 
         Dependencies
         ------------
@@ -150,7 +154,60 @@ def fluxes2mags(
         Comments
         --------
     """
-    mags = -2.5*np.log10(flux/f_ref) + m_ref
+    m = -2.5*np.log10(f/f_ref) + m_ref
 
-    return mags
+    return m
 
+def add_mags(m:np.ndarray, w:np.ndarray=None):
+    """
+        - function to calculate the total magnitude of a set of magnitudes
+
+        Parameters
+        ----------
+            - `m`
+                - np.ndarray
+                - contains magnitudes to add up
+            - `w`
+                - np.ndarray, optional
+                - weight for each passed magnitude
+                    - for example some distance measure
+                - the default is `None`
+                    - will be set to 1 for all elements in `m`
+
+        Raises
+        ------
+
+        Returns
+        -------
+            - `m_tot`
+                - float
+                - combined (weighted) magnitude
+
+        Comments
+        --------
+
+    """
+
+    if w is None: w = np.ones_like(m)
+
+    if m.shape != w.shape:  
+        raise ValueError(
+            f'`m` and `w` have to have same shape but are of shapes {m.shape}, {w.shape}'
+        )
+
+    m_exp = 10**(-0.4*m)
+    m_tot = -2.5*np.log10(w.T@m_exp)
+
+    return m_tot
+
+def contamination(m:np.ndarray, m_cont:np.ndarray):
+    
+    ffrac = mags2fluxes(mag=m_cont, m_ref=m_cont, f_ref=0)
+
+    return
+
+m = np.array([1,1,1,1,1])
+w = None #np.array([1,1])
+
+m_tot = add_mags(m, w=w)
+print(m_tot)
