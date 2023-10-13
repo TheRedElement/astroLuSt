c2133431b970e2a29cc59a41a0635611d4e77631
TheRedElement <lukas.steinwender99@gmail.com>
2023-10-13 15:18:27 +0200

    implemented basic version of add_stars

--- astroLuSt/synthetics/images.py
+++ astroLuSt/synthetics/images.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as sps
 
-from typing import Union, Tuple, Callable
+from astroLuSt.physics import photometry as alpp
+
+from typing import Union, Tuple, Callable, Literal
 
 #%%classes
 class TPF:
@@ -27,28 +29,41 @@ class TPF:
     def add_noise(self,
         amp:float=1,
         ):
-
-        self.frame[:,:,2] += np.random.randn(*self.frame.shape[:2])
+        
+        amp *= 1E-3
+        self.frame[:,:,2] += amp*np.random.randn(*self.frame.shape[:2])
 
         return
     
     def add_stars(self,
-        amp:float=1,
+        pos:Union[np.ndarray,Literal['random']],
+        f:np.ndarray,
         ):
 
-        star = self.star(pos=np.array([10,10]), m=10)
+        if pos == 'random':
+            raise NotImplementedError()
 
-        self.frame[:,:,2] += star
+        for posi, fi in zip(pos,f):
+            star = self.star(pos=posi, f=fi)
+
+            self.frame[:,:,2] += star
 
         return
     
     def star(self,
         pos:np.ndarray,
-        m:Union[float,np.ndarray]=None, f:Union[float,np.ndarray]=None
+        f:float=None, m:float=None, 
         ):
 
-        star = sps.multivariate_normal(
-            mean=pos, cov=m, allow_singular=True
+        if f is None:
+            if m is not None:
+                f =  alpp.mags2fluxes(m=m)
+            else:
+                raise ValueError(f'At least one of `f` and `m` has to be not `None` but they have values {f} and {m}.')
+            
+
+        star = f*sps.multivariate_normal(
+            mean=pos, cov=f, allow_singular=True
         ).pdf(self.frame[:,:,:2])
 
         return star
@@ -58,21 +73,28 @@ class TPF:
 
         fig = plt.figure()
         ax1 = fig.add_subplot(111)
-        mesh = ax1.pcolormesh(self.frame[:,:,0], self.frame[:,:,1], self.frame[:,:,2], cmap='viridis_r')
+        mesh = ax1.pcolormesh(self.frame[:,:,0], self.frame[:,:,1], self.frame[:,:,2], cmap='viridis')
         # ax1.scatter(*star_targ[:2]+.5, marker='x', c='r', label='Target')
 
         ax1.set_xlabel('Pixel')
         ax1.set_ylabel('Pixel')
 
         cbar = fig.colorbar(mesh, ax=ax1)
-        cbar.ax.invert_yaxis()
+        # cbar.ax.invert_yaxis()
         # cbar.ax.set_ylim(0-mag_range/2-1)
-        cbar.set_label('Magnitude [mag]')
+        cbar.set_label('Flux [-]')
 
         ax1.legend()
 
         axs = fig.axes
 
         return fig, axs
+
+
+class TPF_Series:
+
+    def __init__(self) -> None:
+        pass
+
 
 #%%definitions
