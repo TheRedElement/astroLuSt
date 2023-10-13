9efd5f460f1dabb9282dcaa3c6e92bd82a609e64
TheRedElement <lukas.steinwender99@gmail.com>
2023-10-13 15:38:14 +0200

    implemented first version of random starfield

--- astroLuSt/synthetics/images.py
+++ astroLuSt/synthetics/images.py
@@ -37,14 +37,40 @@ class TPF:
     
     def add_stars(self,
         pos:Union[np.ndarray,Literal['random']],
-        f:np.ndarray,
+        f:np.ndarray=None,
+        random_config:dict=None,
         ):
 
-        if pos == 'random':
-            raise NotImplementedError()
+        default_random_config = {
+            'nstars':1,
+            'sizex':self.frame.shape[0],
+            'sizey':self.frame.shape[1],
+            'fmin':0,
+            'fmax':100,
+            'apmin':1,
+            'apmax':10,
+        }
+        if random_config is None:
+            random_config = default_random_config
+        else:
+            for k in random_config.keys(): 
+                default_random_config[k] = random_config[k]
+                random_config = default_random_config
 
-        for posi, fi in zip(pos,f):
-            star = self.star(pos=posi, f=fi)
+
+
+        if pos == 'random':
+            posx        = np.random.choice(range(random_config['sizex']), size=(random_config['nstars'],1))
+            posy        = np.random.choice(range(random_config['sizey']), size=(random_config['nstars'],1))
+            pos         = np.append(posx, posy, axis=1)
+            f           = np.random.choice(range(random_config['fmin'],  random_config['fmax'], 1), size=(random_config['nstars']))
+            aperture    = np.random.choice(range(random_config['apmin'], random_config['apmax'],1), size=(random_config['nstars']))
+        elif pos != 'random' and f is None:
+            raise ValueError("`f` has to be provided is `pos` is not `'random'`")
+            
+
+        for posi, fi, api in zip(pos,f, aperture):
+            star = self.star(pos=posi, f=fi, aperture=api)
 
             self.frame[:,:,2] += star
 
@@ -52,7 +78,8 @@ class TPF:
     
     def star(self,
         pos:np.ndarray,
-        f:float=None, m:float=None, 
+        f:float=None, m:float=None,
+        aperture:float=1,
         ):
 
         if f is None:
@@ -62,8 +89,9 @@ class TPF:
                 raise ValueError(f'At least one of `f` and `m` has to be not `None` but they have values {f} and {m}.')
             
 
+        cov = aperture/2*2.335      #aperture = 2*halfwidth
         star = f*sps.multivariate_normal(
-            mean=pos, cov=f, allow_singular=True
+            mean=pos, cov=cov, allow_singular=True
         ).pdf(self.frame[:,:,:2])
 
         return star
