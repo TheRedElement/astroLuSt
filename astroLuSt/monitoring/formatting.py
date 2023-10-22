da7db4eb7eeaa4b1c762d50ad76c703f6c5f2928
TheRedElement <lukas.steinwender99@gmail.com>
2023-10-22 11:08:20 +0200

    added verbosity to printf()

--- astroLuSt/monitoring/formatting.py
+++ astroLuSt/monitoring/formatting.py
@@ -1,11 +1,12 @@
 
 #%%imports
-import typing
+from typing import Literal
 
 #%%definitions
 def printf(
     msg:str, context:str=None,
-    type:str=None
+    type:Literal['INFO', 'WARNING']=None,
+    verbose:int=0,
     ):
     """
         - function to print a formatted mesage
@@ -20,6 +21,12 @@ def printf(
                 - context to the printed message
                 - the default is `None`
                     - will print `''`
+            - `type`
+                - Literal, optional
+                - type of the message
+                - allowed strings are
+                    - `'INFO'`
+                    - `'WARNING'`
 
         Raises
         ------
@@ -29,17 +36,22 @@ def printf(
 
         Dependencies
         ------------
+            - typing
 
         Comments
         --------
     """
     if context is None: context = ''
     if type is None: type = 'INFO'
+    
+    #determine when and what to show
+    if type == 'INFO':      vbs_th = 2
+    elif type == 'WARNING': vbs_th = 1
 
     to_print = (
         f'{type}({context}): {msg}'
     )
-
-    print(to_print)
+    if verbose >= vbs_th:
+        print(to_print)
 
     return
