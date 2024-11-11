60ef0ad3234f5b80841f49838ea47340ea1a6091
TheRedElement <lukas.steinwender99@gmail.com>
2024-11-11 11:01:25 +0100

    updated summary2pandas_kers to work with current keras verions

--- astroLuSt/datascience/totables.py
+++ astroLuSt/datascience/totables.py
@@ -1,9 +1,11 @@
 
 #%%imports
+import numpy as np
 import pandas as pd
 import re
 from tensorflow.keras import Model
-from typing import Dict
+import tensorflow as tf
+from typing import Dict, List
 
 #%%definitions
 def hypergrid2latex(
@@ -62,9 +64,8 @@ def hypergrid2latex(
     return hypergrid_str
 
 def summary2pandas_keras(
-    model:Model, width:int=250,
-    latex_to_file:str=False, write_mode:str="a",
-    save:str=False,
+    model:Model,
+    layer_attrs:List[str]=None,
     verbose:int=0,
     ) -> pd.DataFrame:
     """
@@ -75,25 +76,11 @@ def summary2pandas_keras(
             - `model`
                 - `tf.keras.Model`
                 - model to extract the summary of
-            - `width`
-                - `int`, optional
-                - width used in the `model.summary()` function
-                - the default is 250
-            - `latex_to_file`
-                - `str`, `bool`, optional
-                - filename of the file to export the latex-representation of the table to
-                - the default is `False`
-                    - will not export the representation
-            - `write_mode`
-                - `str`, optional
-                - mode to use for writing to `latex_to_file`
-                - the default is `a`
-                    - will append to the file
-            - `save`
-                - `str`, optional
-                - path to location of where to save the model summary
-                - the default is `False`
-                    - will not be saved
+            - `layer_attrs`
+                - `List[str]`, optional
+                - additional layer attributes to extract for each layer
+                - the default is `None`
+                    - will be set to `["units"]`
             - `verbose`
                 - `int`, optional
                 - verbosity level
@@ -106,100 +93,48 @@ def summary2pandas_keras(
         -------
             - `df_summary`
                 - `pd.DataFrame`
-                - contains the table created with `model.summary()`
+                - extracted model summary
         
         Comments
         --------
     """
 
-    #rewrite info to list
-    temp = []
-    model.summary(width, print_fn=lambda line: temp.append(line))
-
-    #extract relevant rows
-    # name = temp[0][8:-1]
-    # header = temp[2]
-    data = temp[4:-5]
-    footer = temp[-4:-1]
-
-    
-    #extract entries and format as pandas dataframe    
-    df_summary = pd.DataFrame({
-        (model.name, "Layer"):[],
-        (model.name, "Type"):[],
-        (model.name, "Output Shape"):[],
-        (model.name, "Param #"):[],
-        (model.name, "Activation"):[],
-        (model.name, "Filters"):[],
-        (model.name, "Kernel Size"):[],
-        (model.name, "Units"):[],
-    }, dtype=str)
+    #default parameters
+    if layer_attrs is None: layer_attrs = ["units"]
     
     #extract some additional information for each layer
-    activations = []
-    filters = []
-    kernel_sizes = []
-    units = []
+    extraction = dict(
+        name=[],
+        type=[],
+        input_shape=[], output_shape=[],
+        activation=[],
+        trainable_params=[],
+        non_trainable_params=[],
+        total_params=[],
+    )
+    #get basic layer attributes
     for idx, layer in enumerate(model.layers):
-        if hasattr(layer, "activation"):
-            activations.append(layer.activation.__name__)
-        else:
-            activations.append(None)
-        if hasattr(layer, "filters"):
-            filters.append(layer.filters)
-        else:
-            filters.append(None)
-        if hasattr(layer, "kernel_size"):
-            kernel_sizes.append(layer.kernel_size)
-        else:
-            kernel_sizes.append(None)
-        if hasattr(layer, "units"):
-            units.append(layer.units)
-        else:
-            units.append(None)
+
+        # print(dir(layer))
+        extraction["name"].append(layer.name)
+        extraction["type"].append(layer.__class__.__name__)
+        if hasattr(layer.input, "shape"):   extraction["input_shape"].append(layer.input.shape) 
+        else:                               extraction["input_shape"].append("")
+        if hasattr(layer.output, "shape"):  extraction["output_shape"].append(layer.output.shape) 
+        else:                               extraction["output_shape"].append("")
+        if hasattr(layer, "activation"):    extraction["activation"].append(layer.activation.__name__)
+        else:                               extraction["activation"].append(None)
+        extraction["trainable_params"].append(int(np.sum([tf.size(w) for w in layer.trainable_weights])))
+        extraction["non_trainable_params"].append(int(np.sum([tf.size(w) for w in layer.non_trainable_weights])))
+        extraction["total_params"].append(layer.count_params())
+
+    #get additional attributes
+    for attr in layer_attrs:
+        extraction[attr] = []
+        for layer in model.layers:
+            if hasattr(layer, attr):    extraction[attr].append(eval(f"layer.{attr}"))
+            else:                       extraction[attr].append(None)
+
+    df_summary = pd.DataFrame(extraction)
     
-    for idx, (row, activation, filter, kernel_size, u) in enumerate(zip(
-        [re.split(r"\ {2,}", d) for d in data[::2]],
-        activations,
-        filters,
-        kernel_sizes,
-        units
-        )):
-        
-        #append row to dataframe
-        df_summary.loc[df_summary.shape[0]] = [
-            re.findall(r"[^\s\(\)]+", row[0][:])[0],
-            re.findall(r"[^\s\(\)]+", row[0][1:])[1],
-            eval(row[1]),
-            int(row[2]),
-            activation,
-            filter,
-            kernel_size,
-            u,
-        ]
-    if verbose > 0: print(df_summary)
-
-    #extract paramter summary and format as pandas dataframe
-    df_params = pd.DataFrame({
-        "Total Parameters":[re.sub(r"\,", "", footer[0][14:])],
-        "Trainable Parameters":[re.sub(r"\,", "", footer[1][18:])],
-        "Non-Trainable Parameters":[re.sub(r"\,", "", footer[2][22:])],
-    })
-    if verbose > 0: print(df_params)
-
-    #write latex-table to external file
-    if isinstance(latex_to_file, str):
-        with open(latex_to_file, write_mode) as outfile:
-            outfile.write("\n\n"+"#"*50+"\n")
-            outfile.write(f"Model:{model.name}\n")
-            outfile.write(f"Total Parameters:{df_params['Total Parameters'].iloc[0]}\n")
-            outfile.write(f"Trainable Parameters:{df_params['Trainable Parameters'].iloc[0]}\n")
-            outfile.write(f"Non-Trainable Parameters:{df_params['Non-Trainable Parameters'].iloc[0]}\n")
-            outfile.write(df_summary.to_latex(
-                index=False, position="H",
-                label="tab:YOURLABEL", caption=f"Summary table of {model.name}"))
-
-    if isinstance(save, str): df_summary.to_csv(save, index=False)
-
-
     return df_summary
